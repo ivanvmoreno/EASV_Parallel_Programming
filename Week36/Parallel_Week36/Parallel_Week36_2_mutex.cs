4f2cc230acc3d607af7369f323adf33297e4922d
Iván Moreno <contacto@ivanmoreno.me>
2019-09-11 17:59:28 +0200

    [feature] Week 36 exercises

---
+++ Week36/Parallel_Week36/Parallel_Week36_2_mutex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Parallel_Week36
+{
+    public class Exercise_2_mutex
+    {
+        public int sharedCounter;
+        private Mutex m = new Mutex();
+
+        public static void _Main()
+        {
+            Exercise_2_mutex program = new Exercise_2_mutex();
+
+            Thread thread_1 = new Thread(new ParameterizedThreadStart(program.IncrementCounter));
+            Thread thread_2 = new Thread(new ParameterizedThreadStart(program.IncrementCounter));
+
+            thread_1.Start();
+            thread_2.Start();
+            Console.WriteLine("Starting threads...");
+
+            thread_1.Join();
+            thread_2.Join();
+
+            Console.WriteLine(program.sharedCounter);
+        }
+
+        private void IncrementCounter(object data)
+        {
+            m.WaitOne();
+            for (int i = 0; i < 100000; i++)
+            {
+                sharedCounter++;
+            }
+            m.ReleaseMutex();
+        }
+    }
+}
---
+++ Week36/exercises/Parallel_Week36_2_lock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Parallel_Week36
+{
+    public class Exercise_2_lock
+    {
+        public int sharedCounter;
+        private Object _lockCounter = new Object();
+
+        public static void Main()
+        {
+            Exercise_2_lock program = new Exercise_2_lock();
+
+            Thread thread_1 = new Thread(new ParameterizedThreadStart(program.IncrementCounter));
+            Thread thread_2 = new Thread(new ParameterizedThreadStart(program.IncrementCounter));
+
+            thread_1.Start();
+            thread_2.Start();
+            Console.WriteLine("Starting threads...");
+
+            thread_1.Join();
+            thread_2.Join();
+
+            Console.WriteLine(program.sharedCounter);
+        }
+
+        private void IncrementCounter(object data)
+        {
+            lock (_lockCounter)
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    sharedCounter++;
+                }
+            }
+        }
+    }
+}
