934c186c19345c038f2c301a5ec1ed0e3cd3e8df
Iván Moreno <contacto@ivanmoreno.me>
2019-09-16 20:18:14 +0200

    [feature] Finished Week 36 exercises

--- Week36/exercises/Parallel_Week36_5.cs
+++ Week36/exercises/Parallel_Week36_5.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 
 namespace Parallel_Week36
 {
-    public class Exercise_3
+    public class Exercise_5
     {
         private const int PROD_DELAY = 500;
         private const int CONS_DELAY = 2000;
         private const int ARRAY_SIZE = 10;
         private const int EXIT = -1;
-        private List<int> sharedArray = new List<int>();
-        private SemaphoreSlim cons = new SemaphoreSlim(0);
-        private SemaphoreSlim prod = new SemaphoreSlim(ARRAY_SIZE);
+        private ThreadSafeQueue<int> sharedArray = new ThreadSafeQueue<int>(ARRAY_SIZE);
 
         public static void Main()
         {
-            Exercise_3 program = new Exercise_3();
+            Exercise_5 program = new Exercise_5();
 
             Thread producer = new Thread(() => program.Produce(0, 25));
             Thread consumer_1 = new Thread(() => program.Consume());
@@ -37,32 +34,25 @@ namespace Parallel_Week36
         {
             for (int i = start; i <= end; i++)
             {
-                prod.Wait();
                 sharedArray.Add(i);
-                cons.Release();
                 Console.WriteLine("Added {0}", i);
                 Thread.Sleep(PROD_DELAY);
             }
-            prod.Wait();
+
             sharedArray.Add(EXIT);
-            cons.Release();
             Console.WriteLine("Producer finished");
         }
 
         private void Consume()
         {
             bool done = false;
-            while(!done)
+            while (!done)
             {
-                cons.Wait();
-                int element = sharedArray[0];
-                sharedArray.RemoveAt(0);
-                prod.Release();
+                int element = sharedArray.Remove();
                 if (element == EXIT)
                 {
                     done = true;
                     sharedArray.Add(element);
-                    cons.Release();
                     Console.WriteLine("Consumer {0} finished", Thread.CurrentThread.Name);
                 }
                 else
---
+++ Week36/exercises/ThreadSafeQueue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+namespace Parallel_Week36
+{
+    public class ThreadSafeQueue<T>
+    {
+        private List<T> list;
+        private SemaphoreSlim addSemaphore;
+        private SemaphoreSlim removeSemaphore;
+
+        public ThreadSafeQueue(int size)
+        {
+
+            list = new List<T>();
+            addSemaphore = new SemaphoreSlim(size);
+            removeSemaphore = new SemaphoreSlim(0);
+        }
+
+        public void Add(T element)
+        {
+            addSemaphore.Wait();
+            list.Add(element);
+            removeSemaphore.Release();
+        }
+
+        public T Remove()
+        {
+            removeSemaphore.Wait();
+            T element = list[0];
+            list.RemoveAt(0);
+            addSemaphore.Release();
+            return element;
+        }
+    }
+}
